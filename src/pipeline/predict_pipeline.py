c802ae1647b3ef0329bc39a181723114d0c7d4aa
nikumbhShubham <shubhamnikumbh19@gmail.com>
2025-11-29 17:21:16 +0530

    dockerized the project

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 # from src.logger import logging
@@ -13,8 +14,10 @@ class PredictPipeline:
         
         try:
             
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            # model_path='artifacts\model.pkl'
+            model_path= os.path.join('artifacts','model.pkl')
+            # preprocessor_path='artifacts\preprocessor.pkl'
+            preprocessor_path= os.path.join('artifacts','preprocessor.pkl')
             
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
